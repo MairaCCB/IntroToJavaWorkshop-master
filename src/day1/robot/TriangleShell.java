ef51fdb70092b28bea49eeb17932bfa4bcb5903b
Love4653 <league@WTS-IM-6.attlocal.net>
2017-08-08 11:54:53 -0700

    We controlled a robot.

---
+++ src/day1/robot/TriangleShell.java
@@ -0,0 +1,44 @@
+package day1.robot;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class TriangleShell {
+
+	// 1. Create a new Robot
+Robot m = new Robot();
+	
+	void go() {
+		 // 3. delete this line (used only for testing)
+
+		// 6. Make the robot go as fast as possible
+m.setSpeed(10);
+		// 4. make a variable to hold the length of the triangle and set it to 50
+int length = 50;
+		// 7. Do the following (up to step 10) 60 times
+
+			// 9. Change the color of the pen to a random color
+	
+			// 8. Increase the length of the side by 10 pixels
+	
+			// 5. call your drawTriangle() method using your length variable
+	drawTriangle(length);
+			// 10. Turn the tortoise 6 degrees to the right
+
+	}
+
+	/* 2. fill in the method below to draw a triangle. Use the length variable for the size of the triangle. */
+	private void drawTriangle(int length) {
+		m.penDown();
+		for (int i = 0; i < 3; i++) {
+			m.move(length);
+			m.turn(360/3);
+		}
+		
+		
+	}
+
+	
+	public static void main(String[] args) {
+		new TriangleShell().go();
+	}
+}
